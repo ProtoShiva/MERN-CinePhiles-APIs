45de9413e95c605557a4f62f8697eb50d868f907
Himanshu <hgusain088@gmail.com>
2024-05-31 21:41:31 +0530

    minor change

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express")
 const {
   create,
   verifyEmail,
@@ -6,47 +6,47 @@ const {
   forgetPassword,
   sendResetPasswordTokenStatus,
   resetPassword,
-  signIn,
-} = require("../controllers/user");
-const { isAuth } = require("../middlewares/auth");
-const { isValidPassResetToken } = require("../middlewares/user");
+  signIn
+} = require("../controllers/user")
+const { isAuth } = require("../middlewares/auth")
+const { isValidPassResetToken } = require("../middlewares/user")
 const {
   userValidtor,
   validate,
   validatePassword,
-  signInValidator,
-} = require("../middlewares/validator");
+  signInValidator
+} = require("../middlewares/validator")
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/create", userValidtor, validate, create);
-router.post("/sign-in", signInValidator, validate, signIn);
-router.post("/verify-email", verifyEmail);
-router.post("/resend-email-verification-token", resendEmailVerificationToken);
-router.post("/forget-password", forgetPassword);
+router.post("/create", userValidtor, validate, create)
+router.post("/sign-in", signInValidator, validate, signIn)
+router.post("/verify-email", verifyEmail)
+router.post("/resend-email-verification-token", resendEmailVerificationToken)
+router.post("/forget-password", forgetPassword)
 router.post(
   "/verify-pass-reset-token",
   isValidPassResetToken,
   sendResetPasswordTokenStatus
-);
+)
 router.post(
   "/reset-password",
   validatePassword,
   validate,
   isValidPassResetToken,
   resetPassword
-);
+)
 router.get("/is-auth", isAuth, (req, res) => {
-  const { user } = req;
+  const { user } = req
   res.json({
     user: {
       id: user._id,
       name: user.name,
       email: user.email,
       isVerified: user.isVerified,
-      role: user.role,
-    },
-  });
-});
+      role: user.role
+    }
+  })
+})
 
-module.exports = router;
+module.exports = router
